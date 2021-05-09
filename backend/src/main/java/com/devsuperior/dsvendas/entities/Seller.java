503d044dda7f6a4edd2d46486daed42e368635a3
Peres Julião <peresjuliao@gmail.com>
2021-05-09 00:57:30 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -1,46 +1,52 @@
 package com.devsuperior.dsvendas.entities;
 
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_sellers")
 public class Seller {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+	
+	public Seller() {
+	}
 
-    @OneToMany(mappedBy = "seller")
-    private List<Sale> sales = new ArrayList<>();
+	public Seller(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public Seller() {
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Seller(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Sale> getSales() {
-        return sales;
-    }
+	public List<Sale> getSales() {
+		return sales;
+	}
 }
